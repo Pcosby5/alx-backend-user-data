68357a047df7b1ebdb769ad0a867e1718a367331
Pcosby5 <pcosby50@gmail.com>
2024-05-30 04:16:17 +0200

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for password encryption using bcrypt.
+Module for password encryption and validation using bcrypt.
 """
 
 import bcrypt
@@ -21,3 +21,19 @@ def hash_password(password: str) -> bytes:
     # Hash the password with the generated salt
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The password to validate.
+
+    Returns:
+        bool: True if the password matches the
+        hashed password, False otherwise.
+    """
+    # Check if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,10 +4,23 @@ Main file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
+
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+
+# hash_password = __import__('encrypt_password').hash_password
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+# print(hash_password(password))
 
 # #!/usr/bin/env python3
 # """
