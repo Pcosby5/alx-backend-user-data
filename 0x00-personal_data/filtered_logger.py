54b628859a15a8b46b61d534bc865bb2bbc3cb2e
Pcosby5 <pcosby50@gmail.com>
2024-05-30 02:55:06 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ This module contains a function to obfuscate specified fields in log messages.
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -25,3 +26,29 @@ def filter_datum(fields: List[str],
     pattern = '|'.join([f"{field}=[^{separator}]*" for field in fields])
     return re.sub(pattern,
                   lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Redacts specified fields in the log message.
+
+        Args:
+            record (logging.LogRecord): The log record to format.
+
+        Returns:
+            str: The formatted log record with specified fields redacted.
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
