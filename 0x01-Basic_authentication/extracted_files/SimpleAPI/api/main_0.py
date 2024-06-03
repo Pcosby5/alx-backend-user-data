a1c27424eea996920f5a9136f649a7c860be58ce
Pcosby5 <pcosby50@gmail.com>
2024-06-03 20:15:42 +0200

    basic auth

---
+++ 0x01-Basic_authentication/extracted_files/SimpleAPI/api/main_0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Main 0
+"""
+import sys
+import os
+
+# Ensure the parent directory is in the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from api.v1.auth.auth import Auth
+
+a = Auth()
+
+print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+print(a.authorization_header())
+print(a.current_user())
