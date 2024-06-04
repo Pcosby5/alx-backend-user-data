423eed58466271b2cf06d429b3abfd11c22ead94
Pcosby5 <pcosby50@gmail.com>
2024-06-04 05:40:06 +0200

    set auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns the authorization header from the request """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns the current user """
+        return None
