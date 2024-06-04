14640dc89050bc926bcdcd0ab0be0de410fe0aa2
Pcosby5 <pcosby50@gmail.com>
2024-06-04 15:26:38 +0200

    Basic - Base64 part

--- 0x01-Basic_authentication/api/main_0.py
+++ 0x01-Basic_authentication/api/main_0.py
@@ -7,17 +7,31 @@ import os
 # Ensure the parent directory is in the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
-a = Auth()
+a = BasicAuth()
+
+print(a.extract_base64_authorization_header(None))
+print(a.extract_base64_authorization_header(89))
+print(a.extract_base64_authorization_header("Holberton School"))
+print(a.extract_base64_authorization_header("Basic Holberton"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.extract_base64_authorization_header("Basic1234"))
+
+# from api.v1.auth.auth import Auth
+
+# a = Auth()
 
 # print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
 # print(a.authorization_header())
 # print(a.current_user())
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
+
+
+# print(a.require_auth(None, None))
+# print(a.require_auth(None, []))
+# print(a.require_auth("/api/v1/status/", []))
+# print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+# print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
+# print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
+# print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
