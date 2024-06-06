ef249f0e44e1a18e9e18cae49663e756ba89a910
Pcosby5 <pcosby50@gmail.com>
2024-06-06 11:57:37 +0200

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -21,7 +21,8 @@ class Auth:
 
         Args:
             path (str): The path to check
-            excluded_paths (List[str]): A list of paths that do not require authentication
+            excluded_paths (List[str]): A list of
+            paths that do not require authentication
 
         Returns:
             bool: True if authentication is required, False otherwise
