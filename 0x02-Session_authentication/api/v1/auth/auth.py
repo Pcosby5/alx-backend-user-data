e281bee55f78dd0b68c0ed26c0f350e90872436b
Pcosby5 <pcosby50@gmail.com>
2024-06-07 00:02:58 +0200

    session_cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ Auth class
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 # Define a type variable for user type
 User = TypeVar('User')
@@ -27,19 +28,15 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise
         """
-        if path is None:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
-
-        if not excluded_paths:
-            return True
-
-        # Ensure path and excluded_paths are slash tolerant
-        path = path.rstrip('/') + '/'
 
         for excluded_path in excluded_paths:
-            if excluded_path.rstrip('/') + '/' == path:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
                 return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
@@ -56,7 +53,7 @@ class Auth:
         if request is None:
             return None
 
-        return request.headers.get("Authorization")
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> User:
         """
@@ -70,3 +67,10 @@ class Auth:
             User: None, indicating no user is retrieved
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Return a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
