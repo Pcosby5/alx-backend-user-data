a340d71f89b26a1254b19928fbb652c2bbf9e414
Pcosby5 <pcosby50@gmail.com>
2024-06-07 06:33:30 +0200

    session id for identifying user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,35 +1,37 @@
-#!/usr/bin/env python3
-"""
-SessionAuth class that inherits from Auth
-"""
-
-# api/v1/auth/session_auth.py
-
-from api.v1.auth.auth import Auth
-import uuid
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 from models.user import User
+from os import getenv
 
 
 class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Create a session ID for a user_id"""
+        """ Create a session ID for a user_id """
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = str(uuid.uuid4())
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID"""
+        """ Returns a User ID based on a Session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
+    def session_cookie(self, request=None):
+        """ Returns the cookie value from a request """
+        if request is None:
+            return None
+        session_name = getenv('SESSION_NAME')
+        if session_name is None:
+            return None
+        return request.cookies.get(session_name)
+
     def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
+        """ Returns a User instance based on a cookie value """
         session_id = self.session_cookie(request)
         if session_id is None:
             return None
