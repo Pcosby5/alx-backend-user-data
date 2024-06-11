99d2e549c7f6c3ce6926f3d6504d4bc9ea1060de
Pcosby5 <pcosby50@gmail.com>
2024-06-11 10:01:23 +0200

    auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt and return the hashed password as bytes.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,11 +1,91 @@
 #!/usr/bin/env python3
 """
-Main file to test User model.
+Main file
 """
+from auth import _hash_password
 
-from user import User
+print(_hash_password("Hello Holberton"))
 
-print(User.__tablename__)
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+# from db import DB
+# from user import User
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+# from sqlalchemy.exc import InvalidRequestError
+# from sqlalchemy.orm.exc import NoResultFound
+
+
+# my_db = DB()
+
+# email = 'test@test.com'
+# hashed_password = "hashedPwd"
+
+# user = my_db.add_user(email, hashed_password)
+# print(user.id)
+
+# try:
+#     my_db.update_user(user.id, hashed_password='NewPwd')
+#     print("Password updated")
+# except ValueError:
+#     print("Error")
+
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+# from db import DB
+# from user import User
+
+# from sqlalchemy.exc import InvalidRequestError
+# from sqlalchemy.orm.exc import NoResultFound
+
+
+# my_db = DB()
+
+# user = my_db.add_user("test@test.com", "PwdHashed")
+# print(user.id)
+
+# find_user = my_db.find_user_by(email="test@test.com")
+# print(find_user.id)
+
+# try:
+#     find_user = my_db.find_user_by(email="test2@test.com")
+#     print(find_user.id)
+# except NoResultFound:
+#     print("Not found")
+
+# try:
+#     find_user = my_db.find_user_by(no_email="test@test.com")
+#     print(find_user.id)
+# except InvalidRequestError:
+#     print("Invalid")
+
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+
+# from db import DB
+# from user import User
+
+# my_db = DB()
+
+# user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+# print(user_1.id)
+
+# user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+# print(user_2.id)
+
+# #!/usr/bin/env python3
+# """
+# Main file to test User model.
+# """
+
+# from user import User
+
+# print(User.__tablename__)
+
+# for column in User.__table__.columns:
+#     print("{}: {}".format(column, column.type))
